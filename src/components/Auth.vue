d55b7c39a8934d26b717cbe562df99cdfa84e75a
Valsidalv <arspida@gmail.com>
2022-07-17 16:36:20 +0200

    Added configure function in validation.js

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable -->
 <template>
     <!-- Auth Modal -->
   <div class="fixed z-10 inset-0 overflow-y-auto " id="modal" 
@@ -70,7 +71,8 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab=='register'" v-bind:validation-schema="schema">
+          <vee-form v-show="tab=='register'" v-bind:validation-schema="schema" v-bind:initial-values="userData"
+          v-on:submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -83,49 +85,62 @@
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input type="email"
+              <vee-field type="email" name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email" />
+                 <ErrorMessage class="text-red-600" name="email"/>
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input type="number"
+              <vee-field type="number" name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded" />
+                <ErrorMessage class="text-red-600" name="age"/>
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
+              <vee-field type="password" name="password" v-bind:bales="false" v-slot="{ field, errors }">
+              <input type="text" class="block w-full py-1.5 px-3 text-gray-800 border
+                border-gray-300 transition
+                duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Password" v-bind="field">
+                <div class="text-red-600" v-for="error in errors" v-bind:key="error">
+                  {{error}}
+                </div>
+              </vee-field>
+                <ErrorMessage class="text-red-600" name="password"/>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input type="password"
+              <vee-field type="password" name="confirm_password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password" />
+                <ErrorMessage class="text-red-600" name="confirm_password"/>
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
+              <vee-field as="select" name="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded">
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+                <option value="England">England</option>
+              </vee-field >
+               <ErrorMessage class="text-red-600" name="country"/>
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
-              <label class="inline-block">Accept terms of service</label>
+              <vee-field name="tos" value="1" type="checkbox" 
+              class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
+              <label class="inline-block">Accept terms of service</label><br>
+              <ErrorMessage class="text-red-600" name="tos"/>
             </div>
             <button type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
@@ -148,13 +163,16 @@ export default {
     return {
       tab: 'login',
       schema: {
-        name: 'required',
-        email: '',
-        age: '',
-        password: '',
-        confirm_password: '',
-        country: '',
-        tos: ''
+        name: 'required|min:3|max:50|alfaSpaces',
+        email: 'required|min:3|max:50|email',
+        age: 'required|min_value:18|max_value:100',
+        password: 'required|min:3|max:20',
+        confirm_password: 'password_mismatch:@password',
+        country: 'required|country_excluded:England',
+        tos: 'tos'
+      },
+      userData: {
+        country: 'Mexico'
       }
     };
   },
@@ -165,7 +183,10 @@ export default {
     // })
   },
   methods: {
-    ...mapMutations(['toggleAuthModal'])
+    ...mapMutations(['toggleAuthModal']),
+    register(values) {
+      console.log(values);
+    }
   }
 };
 </script>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,13 +1,63 @@
 /* eslint-disable */
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate';
-import { required } from '@vee-validate/rules';
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+  configure
+} from 'vee-validate';
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alfaSpaces,
+  alpha_spaces,
+  email,
+  min_value as minVal,
+  max_value as maxVal,
+  confirmed,
+  not_one_of as excluded
+} from '@vee-validate/rules';
 
 export default {
-    install(app) {
-        app.use('VeeForm', VeeForm);
-        app.use('VeeField', VeeField);
-        app.use('ErrorMessage', ErrorMessage);
+  install(app) {
+    app.component('VeeForm', VeeForm);
+    app.component('VeeField', VeeField);
+    app.component('ErrorMessage', ErrorMessage);
 
-        defineRule('required', required);
-    }
+    defineRule('required', required);
+    defineRule('tos', required);
+    defineRule('min', min);
+    defineRule('max', max);
+    defineRule('alfaSpaces', alfaSpaces);
+    defineRule('email', email);
+    defineRule('min_value', minVal);
+    defineRule('max_value', maxVal);
+    defineRule('password_mismatch', confirmed);
+    defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+
+    configure({
+      generateMessage: (context) => {
+        const messages = {
+          required: `Field ${context.field} is required.`,
+          min: `Field ${context.field} is too short.`,
+          max: `Field $context.field} is too long.`,
+          alfaSpaces: `Field ${context.field} can only contain letters and spaces.`,
+          email: `Field ${context.field} must valid email.`,
+          min_value: `Field ${context} is too low.`,
+          max_value: `Field ${context} is too high.`,
+          excludded: `You are not allowed to use this value for Field ${context}.`,
+          country_excluded: "We do not accept users from this country.",
+          password_mismatch: "Passwords do not match.",
+          tos: "You must accept the terms of service."
+        };
+
+        let message = messages[context.rule.name] ? message[context.rule.name] :
+          `Field ${context.field} is invalid.`;
+
+        return message;
+      },
+    });
+  }
 }
